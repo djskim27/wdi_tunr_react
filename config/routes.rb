569e45e2347cb887a76e6d30d882527334dfb86e
djskim27 <djs.kim27@gmail.com>
2017-09-05 12:14:43 -0400

    herokuuuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :api do 
-    resources :artist do
+  namespace :api do 
+    resources :artists do
       resources :songs, only: [:index, :show]
     end
   end
